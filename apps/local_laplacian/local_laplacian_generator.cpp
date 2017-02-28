084e3392454be98669f3ec810c4aeac8e2906280
Patricia Suriana <patricia.suriana@gmail.com>
2017-02-28 15:11:19 -0800

    Add image for auto schedule apps

--- apps/local_laplacian/local_laplacian_generator.cpp
+++ apps/local_laplacian/local_laplacian_generator.cpp
@@ -155,7 +155,7 @@ private:
     // Downsample with a 1 3 3 1 filter
     Func downsample(Func f) {
         using Halide::_;
-        Func downx, downy;
+        Func downx("downx"), downy("downy");
         downx(x, y, _) = (f(2*x-1, y, _) + 3.0f * (f(2*x, y, _) + f(2*x+1, y, _)) + f(2*x+2, y, _)) / 8.0f;
         downy(x, y, _) = (downx(x, 2*y-1, _) + 3.0f * (downx(x, 2*y, _) + downx(x, 2*y+1, _)) + downx(x, 2*y+2, _)) / 8.0f;
         return downy;
@@ -164,7 +164,7 @@ private:
     // Upsample using bilinear interpolation
     Func upsample(Func f) {
         using Halide::_;
-        Func upx, upy;
+        Func upx("upx"), upy("upy");
         upx(x, y, _) = 0.25f * f((x/2) - 1 + 2*(x % 2), y, _) + 0.75f * f(x/2, y, _);
         upy(x, y, _) = 0.25f * upx(x, (y/2) - 1 + 2*(y % 2), _) + 0.75f * upx(x, y/2, _);
         return upy;
