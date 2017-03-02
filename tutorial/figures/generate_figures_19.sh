f94a240d728016a34cfbe8204411d5bb1c5e1f71
Patricia Suriana <psuriana@google.com>
2017-03-02 15:33:29 -0800

    Add animations for tutorial 19

---
+++ tutorial/figures/generate_figures_19.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# This script generates the figures for lesson 5
+
+make_gif()
+{
+for f in tmp/frames_*.tif; do convert $f ${f/tif/gif}; done
+gifsicle --delay $2 --colors 256 --loop tmp/frames*.gif > tmp/$1
+convert -layers Optimize tmp/$1 $1
+rm tmp/frames_*if
+}
+
+make -C ../.. bin/HalideTraceViz
+
+rm -rf tmp
+mkdir -p tmp
+
+# Grab a trace
+HL_TRACE=3 HL_TRACE_FILE=$(pwd)/tmp/trace.bin make -C ../.. tutorial_lesson_19_staging_func_or_image_param
+ls tmp/trace.bin
+
+# Local wrapper
+cat tmp/trace.bin | ../../bin/HalideTraceViz -s 300 300 -t 1 -d 10000 -h 10 \
+	-f 'g_local:f_local' 0 10 -1 0 20 1 32 32 1 0 0 1 -l 'g_local:f_local' 'f' 32 32 1 \
+	-f 'g_local:f_local_in_g_local' 0 10 -1 0 20 1 32 180 1 0 0 1 -l 'g_local:f_local_in_g_local' 'f wrapper' 32 180 1 \
+	-f 'g_local' 0 20 -1 0 20 1 180 32 1 0 0 1 -l 'g_local' 'g' 180 32 1  \
+	 |  avconv -f rawvideo -pix_fmt bgr32 -s 300x300 -i /dev/stdin tmp/frames_%04d.tif
+
+make_gif lesson_19_wrapper_local_g.gif 10
+
+cat tmp/trace.bin | ../../bin/HalideTraceViz -s 300 150 -t 1 -d 10000 -h 10 \
+	-f 'h_local:f_local' 0 10 -1 0 20 1 32 32 1 0 0 1 -l 'h_local:f_local' 'f' 32 32 1 \
+	-f 'h_local' 0 20 -1 0 20 1 180 32 1 0 0 1 -l 'h_local' 'h' 180 32 1  \
+	 |  avconv -f rawvideo -pix_fmt bgr32 -s 300x150 -i /dev/stdin tmp/frames_%04d.tif
+
+make_gif lesson_19_wrapper_local_h.gif 10
+
+
+# Global wrapper
+cat tmp/trace.bin | ../../bin/HalideTraceViz -s 300 300 -t 1 -d 10000 -h 10 \
+	-f 'g_global:f_global' 0 10 -1 0 20 1 32 32 1 0 0 1 -l 'g_global:f_global' 'f' 32 32 1 \
+	-f 'g_global:f_global_global_wrapper' 0 10 -1 0 20 1 32 180 1 0 0 1 -l 'g_global:f_global_global_wrapper' 'f wrapper' 32 180 1 \
+	-f 'g_global' 0 20 -1 0 20 1 180 32 1 0 0 1 -l 'g_global' 'g' 180 32 1  \
+	 |  avconv -f rawvideo -pix_fmt bgr32 -s 300x300 -i /dev/stdin tmp/frames_%04d.tif
+
+make_gif lesson_19_wrapper_global_g.gif 10
+
+cat tmp/trace.bin | ../../bin/HalideTraceViz -s 300 300 -t 1 -d 10000 -h 10 \
+	-f 'h_global:f_global' 0 10 -1 0 20 1 32 32 1 0 0 1 -l 'h_global:f_global' 'f' 32 32 1 \
+	-f 'h_global:f_global_global_wrapper' 0 10 -1 0 20 1 32 180 1 0 0 1 -l 'h_global:f_global_global_wrapper' 'f wrapper' 32 180 1 \
+	-f 'h_global' 0 20 -1 0 20 1 180 32 1 0 0 1 -l 'h_global' 'h' 180 32 1  \
+	 |  avconv -f rawvideo -pix_fmt bgr32 -s 300x300 -i /dev/stdin tmp/frames_%04d.tif
+
+make_gif lesson_19_wrapper_global_h.gif 10
+
+
+# Using wrapper to vary the schedules
+cat tmp/trace.bin | ../../bin/HalideTraceViz -s 450 225 -t 1 -d 10000 -h 10 \
+	-f 'f1_sched:g_sched_in_f1_sched' 0 16 -1 0 20 1 32 32 1 0 0 1 -l 'f1_sched:g_sched_in_f1_sched' 'g wrapper' 32 32 1 \
+	-f 'f1_sched' 0 32 -1 0 20 1 225 32 1 0 0 1 -l 'f1_sched' 'f1' 225 32 1  \
+	 |  avconv -f rawvideo -pix_fmt bgr32 -s 450x225 -i /dev/stdin tmp/frames_%04d.tif
+
+make_gif lesson_19_wrapper_vary_schedule_f1.gif 5
+
+cat tmp/trace.bin | ../../bin/HalideTraceViz -s 450 225 -t 1 -d 10000 -h 10 \
+	-f 'f2_sched:g_sched_in_f2_sched' 0 16 -1 0 20 1 32 32 1 0 0 1 -l 'f2_sched:g_sched_in_f2_sched' 'g wrapper' 32 32 1 \
+	-f 'f2_sched' 0 19 -1 0 20 1 225 32 1 0 0 1 -l 'f2_sched' 'f2' 225 32 1  \
+	 |  avconv -f rawvideo -pix_fmt bgr32 -s 450x225 -i /dev/stdin tmp/frames_%04d.tif
+
+make_gif lesson_19_wrapper_vary_schedule_f2.gif 5
+
+
+# Staging loads from Func
+cat tmp/trace.bin | ../../bin/HalideTraceViz -s 450 450 -t 1 -d 10000 -h 10 \
+	-f 'g_load:f_load' 0 16 -1 0 20 1 32 32 1 0 0 1 -l 'g_load:f_load' 'f' 32 32 1 \
+	-f 'g_load:f_load_in_g_load' 0 16 -1 1 20 1 32 225 1 0 0 1 -l 'g_load:f_load_in_g_load' 'f wrapper' 32 225 1 \
+	-f 'g_load' 0 32 -1 0 20 1 225 32 1 0 0 1 -l 'g_load' 'g' 225 32 1  \
+	 |  avconv -f rawvideo -pix_fmt bgr32 -s 450x450 -i /dev/stdin tmp/frames_%04d.tif
+
+make_gif lesson_19_wrapper_stage_func_loads.gif 5
+
+
+# Grouping stages of a Func in one loop nest
+cat tmp/trace.bin | ../../bin/HalideTraceViz -s 450 450 -t 1 -d 10000 -h 10 \
+	-f 'g_group:f_group' 0 24 -1 0 20 1 32 32 1 0 0 1 -l 'g_group:f_group' 'f' 32 32 1 \
+	-f 'g_group:f_group_in_g_group' 0 24 -1 0 20 1 32 225 1 0 0 1 -l 'g_group:f_group_in_g_group' 'f wrapper' 32 225 1 \
+	-f 'g_group' 0 48 -1 0 20 1 225 32 1 0 0 1 -l 'g_group' 'g' 225 32 1  \
+	 |  avconv -f rawvideo -pix_fmt bgr32 -s 450x450 -i /dev/stdin tmp/frames_%04d.tif
+
+make_gif lesson_19_wrapper_group_func_stages.gif 5
+
+
+# Grouping stages of a Func in one loop nest
+cat tmp/trace.bin | ../../bin/HalideTraceViz -s 450 450 -t 1 -d 10000 -h 10 \
+	-f 'f_img:img' 0 16 -1 0 20 1 32 32 1 0 0 1 -l 'f_img:img' 'Image' 32 32 1 \
+	-f 'f_img:img_im_global_wrapper' 0 16 -1 1 20 1 32 225 1 0 0 1 -l 'f_img:img_im_global_wrapper' 'Image wrapper' 32 225 1 \
+	-f 'f_img' 0 32 -1 0 20 1 225 32 1 0 0 1 -l 'f_img' 'f' 225 32 1  \
+	 |  avconv -f rawvideo -pix_fmt bgr32 -s 450x450 -i /dev/stdin tmp/frames_%04d.tif
+
+make_gif lesson_19_wrapper_image_param.gif 5
+
+
+rm -rf tmp
--- tutorial/lesson_19_staging_func_or_image_param.cpp
+++ tutorial/lesson_19_staging_func_or_image_param.cpp
@@ -35,10 +35,10 @@ int main(int argc, char **argv) {
 
     {
         // Consider the following pipeline:
-        Func f("f"), g("g"), h("h");
+        Func f("f_local"), g("g_local"), h("h_local");
         f(x, y) = x + y;
-        g(x, y) = x * y * f(x, y);
-        h(x, y) = 2 + f(x, y);
+        g(x, y) = 2 * f(x, y);
+        h(x, y) = 3 + f(x, y);
         f.compute_root();
         g.compute_root();
         h.compute_root();
@@ -48,16 +48,22 @@ int main(int argc, char **argv) {
         //     f(x, y) = x + y
         // for y:
         //   for x:
-        //     g(x, y) = x * y * f(x, y)
+        //     g(x, y) = 2 * f(x, y)
         // for y:
         //   for x:
-        //     h(x, y) = 2 + f(x, y)
+        //     h(x, y) = 3 + f(x, y)
 
         // Now, let's do the following:
         Func f_in_g = f.in(g);
         f_in_g.compute_root();
         // Equivalently, we could also chain the schedules like so:
         // f.in(g).compute_root();
+
+        g.realize(5, 5);
+        h.realize(5, 5);
+        // See figures/lesson_19_wrapper_local_g.gif and
+        // figures/lesson_19_wrapper_local_h.gif for a visualization of
+        // what this did.
     }
 
     {
@@ -67,8 +73,8 @@ int main(int argc, char **argv) {
         Func f_in_g("f_in_g"), f("f"), g("g"), h("h");
         f(x, y) = x + y;
         f_in_g(x, y) = f(x, y);
-        g(x, y) = x * y * f_in_g(x, y);
-        h(x, y) = 2 + f(x, y);
+        g(x, y) = 2 * f_in_g(x, y);
+        h(x, y) = 3 + f(x, y);
         f.compute_root();
         g.compute_root();
         h.compute_root();
@@ -81,20 +87,20 @@ int main(int argc, char **argv) {
         //     f_in_g(x, y) = f(x, y)
         // for y:
         //   for x:
-        //     g(x, y) = x * y * f_in_g(x, y)
+        //     g(x, y) = 2 * f_in_g(x, y)
         // for y:
         //   for x:
-        //     h(x, y) = 2 + f(x, y)
+        //     h(x, y) = 3 + f(x, y)
 
         // Note that only calls to 'f' inside 'g' are replaced. The ones inside
         // 'h' remain unchanged.
     }
 
     {
-        Func f("f"), g("g"), h("h");
+        Func f("f_global"), g("g_global"), h("h_global");
         f(x, y) = x + y;
-        g(x, y) = x * y * f(x, y);
-        h(x, y) = 2 + f(x, y);
+        g(x, y) = 2 * f(x, y);
+        h(x, y) = 3 + f(x, y);
         f.compute_root();
         g.compute_root();
         h.compute_root();
@@ -113,20 +119,26 @@ int main(int argc, char **argv) {
         //     f_global_wrapper(x, y) = f(x, y)
         // for y:
         //   for x:
-        //     g(x, y) = x * y * f_global_wrapper(x, y)
+        //     g(x, y) = 2 * f_global_wrapper(x, y)
         // for y:
         //   for x:
-        //     h(x, y) = 2 + f_global_wrapper(x, y)
+        //     h(x, y) = 3 + f_global_wrapper(x, y)
+
+        g.realize(5, 5);
+        h.realize(5, 5);
+        // See figures/lesson_19_wrapper_global_g.gif and
+        // figures/lesson_19_wrapper_global_h.gif for a visualization of
+        // what this did.
     }
 
 
     // Func::in and ImageParam::in can be used for variety of scheduling tricks.
     {
         // Say we have the following pipeline:
-        Func g("g"), f1("f1"), f2("f2");
+        Func g("g_sched"), f1("f1_sched"), f2("f2_sched");
         g(x, y) = x + y;
-        f1(x, y) = x * y * g(x, y);
-        f2(x, y) = x - y + g(x, y);
+        f1(x, y) = 2 * g(x, y);
+        f2(x, y) = 3 + g(x, y);
         // and we want to schedule 'g' differently depending on whether it is
         // used by 'f1' or 'f2': in 'f1', we want to vectorize 'g' across the
         // x dimension, while in 'f2', we want to parallelize it. It is possible
@@ -135,43 +147,47 @@ int main(int argc, char **argv) {
         // g.in(f1) replaces all calls to 'g' inside 'f1' with a unique wrapper
         // Func and then returns that wrapper. Then, we schedule the wrapper to
         // be computed at 'f1' and vectorize it across the x dimension:
-        g.in(f1).compute_at(f1, y).vectorize(x, 8);
+        g.in(f1).compute_at(f1, y).vectorize(x, 4);
 
         // Similarly, to parallelize 'g' across the x dimension inside 'f2', we
         // do the following:
         g.in(f2).compute_at(f2, y).parallel(x);
 
-        Buffer<int> halide_result_f1 = f1.realize(40, 40);
-        Buffer<int> halide_result_f2 = f2.realize(40, 40);
+        Buffer<int> halide_result_f1 = f1.realize(8, 8);
+        Buffer<int> halide_result_f2 = f2.realize(8, 8);
+
+        // See figures/lesson_19_wrapper_vary_schedule_f1.gif and
+        // figures/lesson_19_wrapper_vary_schedule_f2.gif for a visualization
+        // of what this did.
 
         // The equivalent C is:
-        int c_result_f1[40][40];
-        for (int f1_y = 0; f1_y < 40; f1_y++) {
-            int g_in_f1[40];
-            for (int g_x_o = 0; g_x_o < 5; g_x_o++) {
-                /* vectorized */ for (int g_x_i = 0; g_x_i < 8; g_x_i++) {
-                    g_in_f1[8 * g_x_o + g_x_i] = 8 * g_x_o + g_x_i + f1_y;
+        int c_result_f1[8][8];
+        for (int f1_y = 0; f1_y < 8; f1_y++) {
+            int g_in_f1[8];
+            for (int g_x_o = 0; g_x_o < 2; g_x_o++) {
+                /* vectorized */ for (int g_x_i = 0; g_x_i < 4; g_x_i++) {
+                    g_in_f1[4 * g_x_o + g_x_i] = 4 * g_x_o + g_x_i + f1_y;
                 }
             }
-            for (int f1_x = 0; f1_x < 40; f1_x++) {
-                c_result_f1[f1_y][f1_x] = f1_x * f1_y * g_in_f1[f1_x];
+            for (int f1_x = 0; f1_x < 8; f1_x++) {
+                c_result_f1[f1_y][f1_x] = 2 * g_in_f1[f1_x];
             }
         }
 
-        int c_result_f2[40][40];
-        for (int f2_y = 0; f2_y < 40; f2_y++) {
-            int g_in_f2[40];
-            /* parallel */ for (int g_x = 0; g_x < 40; g_x++) {
+        int c_result_f2[8][8];
+        for (int f2_y = 0; f2_y < 8; f2_y++) {
+            int g_in_f2[8];
+            /* parallel */ for (int g_x = 0; g_x < 8; g_x++) {
                 g_in_f2[g_x] = g_x + f2_y;
             }
-            for (int f2_x = 0; f2_x < 40; f2_x++) {
-                c_result_f2[f2_y][f2_x] = f2_x - f2_y + g_in_f2[f2_x];
+            for (int f2_x = 0; f2_x < 8; f2_x++) {
+                c_result_f2[f2_y][f2_x] = 3 + g_in_f2[f2_x];
             }
         }
 
         // Check the results match:
-        for (int y = 0; y < 40; y++) {
-            for (int x = 0; x < 40; x++) {
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 if (halide_result_f1(x, y) != c_result_f1[y][x]) {
                     printf("halide_result_f1(%d, %d) = %d instead of %d\n",
                            x, y, halide_result_f1(x, y), c_result_f1[y][x]);
@@ -179,8 +195,8 @@ int main(int argc, char **argv) {
                 }
             }
         }
-        for (int y = 0; y < 40; y++) {
-            for (int x = 0; x < 40; x++) {
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 if (halide_result_f2(x, y) != c_result_f2[y][x]) {
                     printf("halide_result_f2(%d, %d) = %d instead of %d\n",
                            x, y, halide_result_f2(x, y), c_result_f2[y][x]);
@@ -198,47 +214,50 @@ int main(int argc, char **argv) {
         // places. We don't want to recompute 'f' often, but since it is large,
         // it does not fit in the cache. To deal with this issue, we can do
         // the following:
-        Func f("f"), g("g");
+        Func f("f_load"), g("g_load");
         f(x, y) = x + y;
         g(x, y) = 2 * f(y, x);
         // First, compute 'f' at root.
         f.compute_root();
         // Then, we use Func::in to stage the loads from 'f' in tiles
         // as necessary:
-        Var xi("xi"), yi("yi");
-        g.tile(x, y, xi, yi, 8, 8);
-        f.in(g).compute_at(g, x);
+        Var xo("xo"), xi("xi"), yo("yo"), yi("yi");
+        g.tile(x, y, xo, yo, xi, yi, 4, 4);
+        f.in(g).compute_at(g, xo);
 
-        Buffer<int> halide_result = g.realize(40, 40);
+        Buffer<int> halide_result = g.realize(8, 8);
+
+        // See figures/lesson_19_wrapper_stage_func_loads.gif for a visualization of
+        // what this did.
 
         // The equivalent C is:
-        int f_buffer[40][40];
-        for (int y = 0; y < 40; y++) {
-            for (int x = 0; x < 40; x++) {
+        int f_buffer[8][8];
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 f_buffer[y][x] = x + y;
             }
         }
 
-        int c_result[40][40];
-        for (int y = 0; y < 40; y++) {
-            for (int x = 0; x < 40; x++) {
-                int f_in_g[8][8];
-                for (int yi = 0; yi < 8; yi++) {
-                    for (int xi = 0; xi < 8; xi++) {
-                        f_in_g[y][x] = f_buffer[y][x];
+        int c_result[8][8];
+        for (int yo = 0; yo < 2; yo++) {
+            for (int xo = 0; xo < 2; xo++) {
+                int f_in_g[4][4];
+                for (int yi = 0; yi < 4; yi++) {
+                    for (int xi = 0; xi < 4; xi++) {
+                        f_in_g[yi][xi] = f_buffer[4 * yo + yi][4 * xo + xi];
                     }
                 }
-                for (int yi = 0; yi < 8; yi++) {
-                    for (int xi = 0; xi < 8; xi++) {
-                        c_result[y][x] = 2 * f_in_g[y][x];
+                for (int yi = 0; yi < 4; yi++) {
+                    for (int xi = 0; xi < 4; xi++) {
+                        c_result[4 * yo + yi][4 * xo + xi] = 2 * f_in_g[yi][xi];
                     }
                 }
             }
         }
 
         // Check the results match:
-        for (int y = 0; y < 40; y++) {
-            for (int x = 0; x < 40; x++) {
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 if (halide_result(x, y) != c_result[y][x]) {
                     printf("halide_result(%d, %d) = %d instead of %d\n",
                            x, y, halide_result(x, y), c_result[y][x]);
@@ -251,10 +270,10 @@ int main(int argc, char **argv) {
     {
         // Func::in can also be used to group multiple stages of a Func
         // in the same loop nest. Consider the following code:
-        Func f("f"), g("g");
+        Func f("f_group"), g("g_group");
         f(x, y) = x + y;
         f(x, y) += x - y;
-        g(x, y) = x * y * f(x, y);
+        g(x, y) = 2 * f(x, y);
 
         // When we schedule 'f' to be computed at root (by calling f.compute_root()),
         // all its stages are computed at separate loop nest:
@@ -266,7 +285,7 @@ int main(int argc, char **argv) {
         //     f(x, y) += x - y
         // for y:
         //   for x:
-        //     g(x, y) = x * y * f(x, y)
+        //     g(x, y) = 2 * f(x, y)
 
         // We can use Func::in to group those stages to be computed at the
         // same loop nest like so:
@@ -284,27 +303,30 @@ int main(int argc, char **argv) {
         //     f(x, y) += x - y
         //     f_in_g(x, y) = f(x, y)
 
-        Buffer<int> halide_result = g.realize(20, 20);
+        Buffer<int> halide_result = g.realize(8, 8);
+
+        // See figures/lesson_19_wrapper_group_func_stages.gif for a visualization of
+        // what this did.
 
         // The equivalent C is:
-        int f_in_g[20][20];
-        for (int y = 0; y < 20; y++) {
-            for (int x = 0; x < 20; x++) {
+        int f_in_g[8][8];
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 int f = x + y;
                 f += x - y;
                 f_in_g[y][x] = f;
             }
         }
-        int c_result_g[20][20];
-        for (int y = 0; y < 20; y++) {
-            for (int x = 0; x < 20; x++) {
-                c_result_g[y][x] = x * y * f_in_g[y][x];
+        int c_result_g[8][8];
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                c_result_g[y][x] = 2 * f_in_g[y][x];
             }
         }
 
         // Check the results match:
-        for (int y = 0; y < 20; y++) {
-            for (int x = 0; x < 20; x++) {
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 if (halide_result(x, y) != c_result_g[y][x]) {
                     printf("halide_result(%d, %d) = %d instead of %d\n",
                            x, y, halide_result(x, y), c_result_g[y][x]);
@@ -322,15 +344,15 @@ int main(int argc, char **argv) {
         // The following example illustrates how you would use ImageParam::in
         // to stage loads from an ImageParam in tiles.
         ImageParam img(Int(32), 2, "img");
-        Func f("f");
+        Func f("f_img");
         f(x, y) = 2 * img(y, x);
 
-        // First, we tile 'f' into 8x8 blocks.
-        Var xi("xi"), yi("yi");
-        f.compute_root().tile(x, y, xi, yi, 8, 8);
+        // First, we tile 'f' into 4x4 blocks.
+        Var xo("xo"), xi("xi"), yo("yo"), yi("yi");
+        f.compute_root().tile(x, y, xo, yo, xi, yi, 4, 4);
         // Then, we create a wrapper for 'img' which will load the corresponding
         // values from 'img' at tile level.
-        Func img_wrapper = img.in().compute_at(f, x);
+        Func img_wrapper = img.in().compute_at(f, xo);
         // If, for some reason, we want to unroll the first dimension of the
         // wrapper by 2, we can do the following:
         img_wrapper.unroll(_0, 2);
@@ -339,41 +361,44 @@ int main(int argc, char **argv) {
         // implicit variables to name the dimensions of the image wrapper:
         // _0 as the 1st dimension, _1 as the 2nd dimension, and so on.
 
-        Buffer<int> input(40, 40);
-        for (int y = 0; y < 40; y++) {
-            for (int x = 0; x < 40; x++) {
+        Buffer<int> input(8, 8);
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 input(x, y) = x + y;
             }
         }
         img.set(input);
-        Buffer<int> halide_result = f.realize(40, 40);
+        Buffer<int> halide_result = f.realize(8, 8);
+
+        // See figures/lesson_19_wrapper_image_param.gif for a visualization of
+        // what this did.
 
         // The equivalent C is:
-        int c_result[40][40];
-        for (int f_y = 0; f_y < 5; f_y++) {
-            for (int f_x = 0; f_x < 5; f_x++) {
-                int imgw[8][8];
-                for (int imgw_yi = 0; imgw_yi < 8; imgw_yi++) {
-                    for (int imgw_xi = 0; imgw_xi < 4; imgw_xi++) {
-                        int x = 8 * f_x + 2 * imgw_xi;
-                        int y = 8 * f_y + imgw_yi;
+        int c_result[8][8];
+        for (int f_yo = 0; f_yo < 2; f_yo++) {
+            for (int f_xo = 0; f_xo < 2; f_xo++) {
+                int imgw[4][4];
+                for (int imgw_yi = 0; imgw_yi < 4; imgw_yi++) {
+                    for (int imgw_xi = 0; imgw_xi < 2; imgw_xi++) {
+                        int x = 4 * f_xo + 2 * imgw_xi;
+                        int y = 4 * f_yo + imgw_yi;
                         imgw[imgw_yi][2 * imgw_xi] = input(x, y);
                         imgw[imgw_yi][2 * imgw_xi + 1] = input(x + 1, y);
                     }
                 }
-                for (int f_yi = 0; f_yi < 8; f_yi++) {
-                    for (int f_xi = 0; f_xi < 8; f_xi++) {
-                        int x = 8 * f_x + f_xi;
-                        int y = 8 * f_y + f_yi;
-                        c_result[y][x] = 2 * imgw[f_yi][f_xi];
+                for (int f_yoi = 0; f_yoi < 4; f_yoi++) {
+                    for (int f_xoi = 0; f_xoi < 4; f_xoi++) {
+                        int x = 4 * f_xo + f_xoi;
+                        int y = 4 * f_yo + f_yoi;
+                        c_result[y][x] = 2 * imgw[f_yoi][f_xoi];
                     }
                 }
             }
         }
 
         // Check the results match:
-        for (int y = 0; y < 40; y++) {
-            for (int x = 0; x < 40; x++) {
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 if (halide_result(x, y) != c_result[y][x]) {
                     printf("halide_result(%d, %d) = %d instead of %d\n",
                            x, y, halide_result(x, y), c_result[y][x]);
